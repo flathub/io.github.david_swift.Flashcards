c3362464fd1c2645b9e077d7564e4ba867ca4eb5
david-swift <haenerdavid@icloud.com>
2024-05-20 13:46:36 +0200

    Version 0.2.3

--- setup-offline.sh
+++ setup-offline.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 mkdir .build/repositories
 cd .build/repositories
-mkdir ./Adwaita-39caa65a
-cp -r ../checkouts/Adwaita/.git/* ./Adwaita-39caa65a
 mkdir ./Adwaita-efb31624
 cp -r ../checkouts/Adwaita/.git/* ./Adwaita-efb31624
-mkdir ./LevenshteinTransformations-222f7af2
-cp -r ../checkouts/LevenshteinTransformations/.git/* ./LevenshteinTransformations-222f7af2
 mkdir ./LevenshteinTransformations-2e99b7c3
 cp -r ../checkouts/LevenshteinTransformations/.git/* ./LevenshteinTransformations-2e99b7c3
-mkdir ./Localized-2b72df47
-cp -r ../checkouts/Localized/.git/* ./Localized-2b72df47
 mkdir ./Localized-f70fe787
 cp -r ../checkouts/Localized/.git/* ./Localized-f70fe787
-mkdir ./swift-macro-toolkit-40d6b9db
-cp -r ../checkouts/swift-macro-toolkit/.git/* ./swift-macro-toolkit-40d6b9db
-mkdir ./swift-macro-toolkit-c3e779b9
-cp -r ../checkouts/swift-macro-toolkit/.git/* ./swift-macro-toolkit-c3e779b9
-mkdir ./swift-syntax-e33d5ec5
-cp -r ../checkouts/swift-syntax/.git/* ./swift-syntax-e33d5ec5
-mkdir ./swift-syntax-463e60c9
-cp -r ../checkouts/swift-syntax/.git/* ./swift-syntax-463e60c9
-mkdir ./Yams-81702b8f
-cp -r ../checkouts/Yams/.git/* ./Yams-81702b8f
 mkdir ./Yams-fd9f2519
 cp -r ../checkouts/Yams/.git/* ./Yams-fd9f2519
